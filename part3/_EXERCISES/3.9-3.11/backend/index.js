c3503c728bff9cbf6b75cf99782c3a8ef9a75e27
Sean Kennedy <sean@Seans-MacBook-Air.local>
2025-08-21 08:34:52 -0500

    Creating the dist front end

--- part3/_EXERCISES/3.9-3.11/backend/index.js
+++ part3/_EXERCISES/3.9-3.11/backend/index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static('dist'))
 
 let notes = [  {    id: "1",    content: "HTML is easy",    important: true  },  {    id: "2",    content: "Browser can execute only JavaScript",    important: false  },  {    id: "3",    content: "GET and POST are the most important methods of HTTP protocol",    important: true  }]
 
